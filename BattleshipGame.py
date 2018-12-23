3fff9c0c68d7e74df55f9d8c211150700bafe7de
Bruno Maciel <brunomaciel.xc4@gmail.com>
2018-12-23 18:37:20 -0200

    Add function to set ships on board

--- BattleshipGame.py
+++ BattleshipGame.py
@@ -13,7 +13,7 @@ battleship.definePosition()
 carrier = Ship(5, 'v', "R", "carrier")
 carrier.definePosition()
 
-# make the list in a descending size-sorted, cuz that's the order the ships will be placed on the board
+# list must be in a descending size-sorted, cuz that's the order the ships will be placed on the board
 shipsList = [carrier, battleship, submarine, cruiser, destroyer]
 #end
 
@@ -23,3 +23,66 @@ answerBoard = Board("respostas")
 
 gameBoard.defineBoard()
 #end
+
+### Function to set ships on answerBoard ###
+def set_ship_on_board(ship):
+    # Define ships' position on the board_answer
+    ship_row = ship.bottom_row-1
+    ship_col = ship.bottom_col-1
+    if ship.getOrientation() == 'h':
+        if answerBoard.tabuleiro[ship_row][ship_col] == "_":
+            answerBoard.set_mark_on_board(ship_row,ship_col, ship.mark)
+            # To know the position of first piece of ship, uncomment the line below
+            #print ("row: ",ship_row, " col: ", ship_col)
+        else:
+            answerBoard.eraseMark(ship.mark)
+            return False
+
+        for pieces in range(1,ship.getSize()):
+            ship_col += 1
+            if answerBoard.tabuleiro[ship_row][ship_col] == "_":
+                answerBoard.set_mark_on_board(ship_row, ship_col, ship.mark)
+                # To know the position of first piece of ship, uncomment the line below
+                # print ("row: ",ship_row, " col: ", ship_col)
+            else:
+                answerBoard.eraseMark(ship.mark)
+                return False
+
+    elif ship.getOrientation() == "v":
+        if answerBoard.tabuleiro[ship_row][ship_col] == "_":
+            answerBoard.set_mark_on_board(ship_row, ship_col, ship.mark)
+            # To know the position of first piece of ship, uncomment the line below
+            #print ("row: ",ship_row, " col: ", ship_col)
+        else:
+            answerBoard.eraseMark(ship.mark)
+            return False
+
+        for pieces in range(1,ship.getSize()):
+            ship_row += 1
+            if answerBoard.tabuleiro[ship_row][ship_col] == "_":
+                answerBoard.set_mark_on_board(ship_row, ship_col, ship.mark)
+                # To know the position of first piece of ship, uncomment the line below
+                # print ("row: ",ship_row, " col: ", ship_col)
+            else:
+                answerBoard.eraseMark(ship.mark)
+                return False
+    return True
+
+
+def reposition(check, ship):
+    # Reset the ship on board in case it was overlaping another ship
+    while check != True:
+        # If wanna know how many times the ship was overlapping another and had to be reset, uncomment below
+        #print("overlapping: ", ship.getName())
+        ship.definePosition()
+        check = set_ship_on_board(ship)
+
+def setAllShips_onBoard():
+    for ship in shipsList:
+        success = set_ship_on_board(ship)
+        if success != True:
+            reposition(success, ship)
+    answerBoard.printBoard()
+#end
+
+setAllShips_onBoard()
