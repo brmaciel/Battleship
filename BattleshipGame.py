284828797cd8e547ffc905f36bd938d649c2acb6
Bruno Maciel <brunomaciel.xc4@gmail.com>
2018-12-30 21:27:56 -0200

    Pequenos ajustes de boas praticas para python 3.0

--- BattleshipGame.py
+++ BattleshipGame.py
@@ -1,21 +1,20 @@
-from Board import *
 from Ship import *
 
 ###   Initialize Objects   ###
 # Create all ships
-destroyer = Ship(2, 'v', "D", "Destroyer")
-cruiser = Ship(3, 'h', "C", "Cruiser")
-submarine = Ship(3, 'v', "S", "Submarine")
-battleship = Ship(4, 'h', "B", "Battleship")
-carrier = Ship(5, 'v', "R", "Carrier")
+destroyer = Ship(2, 'v', 'D', 'Destroyer')
+cruiser = Ship(3, 'h', 'C', 'Cruiser')
+submarine = Ship(3, 'v', 'S', 'Submarine')
+battleship = Ship(4, 'h', 'B', 'Battleship')
+carrier = Ship(5, 'v', 'R', 'Carrier')
 
 # list must be in a descending size-sorted, cuz that's the order the ships will be placed on the board
 shipsList = [carrier, battleship, submarine, cruiser, destroyer]
 # end
 
 # Create the gameBoard and the answerBoard
-gameBoard = Board("jogo")
-answerBoard = Board("respostas")
+gameBoard = Board('jogo')
+answerBoard = Board('respostas')
 # end
 
 
@@ -25,39 +24,39 @@ def set_ship_on_board(ship):
     ship_row = ship.row - 1
     ship_col = ship.col - 1
     if ship.orientation == 'h':
-        if answerBoard.getBoardPosition(ship_row, ship_col) == "_":
+        if answerBoard.getBoardPosition(ship_row, ship_col) == '_':
             answerBoard.setMark_on_board(ship_row, ship_col, ship.mark)
             # To know the position of first piece of ship, uncomment the line below
-            # print ("row: ",ship_row, " col: ", ship_col)
+            #print('row: {} col: {}'.format(ship_row, ship_col))
         else:
             answerBoard.eraseMark(ship.mark)
             return False
 
         for pieces in range(1, ship.size):
             ship_col += 1
-            if answerBoard.getBoardPosition(ship_row, ship_col) == "_":
+            if answerBoard.getBoardPosition(ship_row, ship_col) == '_':
                 answerBoard.setMark_on_board(ship_row, ship_col, ship.mark)
                 # To know the position of first piece of ship, uncomment the line below
-                # print ("row: ",ship_row, " col: ", ship_col)
+                #print('row: {} col: {}'.format(ship_row, ship_col))
             else:
                 answerBoard.eraseMark(ship.mark)
                 return False
 
-    elif ship.orientation == "v":
-        if answerBoard.getBoardPosition(ship_row, ship_col) == "_":
+    elif ship.orientation == 'v':
+        if answerBoard.getBoardPosition(ship_row, ship_col) == '_':
             answerBoard.setMark_on_board(ship_row, ship_col, ship.mark)
             # To know the position of first piece of ship, uncomment the line below
-            # print ("row: ",ship_row, " col: ", ship_col)
+            #print('row: {} col: {}'.format(ship_row, ship_col))
         else:
             answerBoard.eraseMark(ship.mark)
             return False
 
         for pieces in range(1, ship.size):
             ship_row += 1
-            if answerBoard.getBoardPosition(ship_row, ship_col) == "_":
+            if answerBoard.getBoardPosition(ship_row, ship_col) == '_':
                 answerBoard.setMark_on_board(ship_row, ship_col, ship.mark)
                 # To know the position of first piece of ship, uncomment the line below
-                # print ("row: ",ship_row, " col: ", ship_col)
+                #print('row: {} col: {}'.format(ship_row, ship_col))
             else:
                 answerBoard.eraseMark(ship.mark)
                 return False
@@ -68,7 +67,7 @@ def reposition(check, ship):
     # Reset the ship on board in case it was overlapping another ship
     while check != True:
         # If wanna know how many times the ship was overlapping another and had to be reset, uncomment below
-        # print("overlapping: ", ship.getName())
+        #print('overlapping: ', ship.name)
         ship.definePosition()
         check = set_ship_on_board(ship)
 
@@ -89,11 +88,11 @@ def checkInput(userInput, opt):
     # input must be: (an integer) AND (between 1 and board size)
     while (userInput.isdigit() == False) or (int(userInput) > gameBoard.size) or (int(userInput) < 1):
         if not userInput.isdigit():
-            print("Error: You must enter numbers only")
-            userInput = input("Pick a %s [1~%d]: " % (opt, gameBoard.size))
+            print('Error: You must enter numbers only')
+            userInput = input('Pick a {} [1~{}]: '.format(opt, gameBoard.size)).strip()
         else:
-            print("Oops, that's not even in the ocean. Pick a row between 1 and %d" % gameBoard.size)
-            userInput = input("Pick a %s [1~%d]: " % (opt, gameBoard.size))
+            print("Oops, that's not even in the ocean. Pick a row between 1 and {}".format(gameBoard.size))
+            userInput = input('Pick a {} [1~{}]: '.format(opt, gameBoard.size)).strip()
     return int(userInput)
 # end
 
@@ -104,15 +103,15 @@ def shipHit(mark):
             ship.hitTaken()
             if ship.getLife == 0:
                 # if the ship hit gets life to zero, it means it was destroyed
-                print("*** Hooyah! %s was destroyed ***" % ship.name)
+                print('*** Hooyah! {} was destroyed ***'.format(ship.name))
 # end
 
 
 ###   Main Function to Start the Game   ###
 def startGame():
     setAllShips_onBoard()
-    print("\n\t\tBattleship: The Game")
-    print(" ===== The Battle has Started ===== \n")
+    print('\n\t\tBattleship: The Game')
+    print(' ===== The Battle has Started ===== \n')
     gameBoard.printBoard()
 
     num_tries = 34
@@ -126,22 +125,22 @@ def startGame():
         for ship in shipsList:
             if ship.getLife != 0:
                 shipsAlive.append(ship.name)
-        print("Ships remaining: ", shipsAlive)
+        print('Ships remaining: ', shipsAlive)
 
-        print("Turn: %d/%d" % (turn + 1, num_tries))
-        print("Pieces Left: ", piecesLeft)  # how many pieces os ships weren't hit yet
-        rowPicked = input("Pick a row [1~%d]: " % gameBoard.size)
-        rowPicked = checkInput(rowPicked, "row")
-        colPicked = input("Pick a col [1~%d]: " % gameBoard.size)
-        colPicked = checkInput(colPicked, "col")
+        print('Turn: {}/{}'.format(turn + 1, num_tries))
+        print('Pieces Left: ', piecesLeft)  # how many pieces os ships weren't hit yet
+        rowPicked = input('Pick a row [1~{}]: '.format(gameBoard.size)).strip()
+        rowPicked = checkInput(rowPicked, 'row')
+        colPicked = input('Pick a col [1~{}]: '.format(gameBoard.size)).strip()
+        colPicked = checkInput(colPicked, 'col')
 
         if gameBoard.getBoardPosition(rowPicked - 1, colPicked - 1) == 'o':
             # 'o' mark on gameBoard means places already picked
-            print("  You picked that one already. You lost a try")
+            print('  You picked that one already. You lost a try')
         elif answerBoard.getBoardPosition(rowPicked - 1, colPicked - 1) != '_' and answerBoard.getBoardPosition(rowPicked - 1, colPicked - 1) != 'x':
             # '_' marker on answerBoard means it's water
             # 'x' marker on answerBoard means it's water and has already been picked
-            print("  # Hooyah! - Battleship hit! #")
+            print('  # Hooyah! - Battleship hit! #')
             gameBoard.setMark_on_board(rowPicked - 1, colPicked - 1, 'o')
             piecesLeft -= 1
 
@@ -149,10 +148,10 @@ def startGame():
 
         elif answerBoard.getBoardPosition(rowPicked - 1, colPicked - 1) == 'x':
             # 'x' on answerBoard means place has a ship but was already picked
-            print("  You picked that one already. You lost a try")
+            print('  You picked that one already. You lost a try')
         else:
             # hit the water, so mark the boards with 'x' to mark the place as 'already picked'
-            print("  # Water! You missed the battleship! #")
+            print('  # Water! You missed the battleship! #')
             gameBoard.setMark_on_board(rowPicked - 1, colPicked - 1, 'x')
             answerBoard.setMark_on_board(rowPicked - 1, colPicked - 1, 'x')
 
@@ -160,15 +159,15 @@ def startGame():
 
         if piecesLeft == 0:
             # All ships were destroyed
-            print("\t###   You Won!   ###")
-            print("Location of Ships:")
+            print('\t###   You Won!   ###')
+            print('Location of Ships:')
             answerBoard.printBoard()
             return 0
 
         if turn == num_tries - 1:
             # Se esgotou o numero de tentativas
-            print("\t###   Game Over!   ###")
-            print("Location of Ships:")
+            print('\t###   Game Over!   ###')
+            print('Location of Ships:')
             answerBoard.printBoard()
 
     return 0
